b0dce0976b723cbf344da60144a1bd6343155dfc
ShotaTevdorasvhili <117285166+Sh0utaa@users.noreply.github.com>
2025-06-29 17:49:39 +0400

    Working router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: '**', redirectTo: 'home' }
+];
