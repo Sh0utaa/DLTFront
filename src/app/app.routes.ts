18f5c27f9e0b6876ff296c9d06bfa407d6a989d0
ShotaTevdorasvhili <stevdorashvili08@gmail.com>
2025-07-01 00:43:56 +0400

    Tailwind css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Login } from './login/login';
-import { Register } from './register/register';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'home', component: Home },
+  { path: 'about', component: About },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: '' } 
 ];
