82513df713f2a3a56279cbbf746ad252749b7830
Sh0utaa <shotatevdorashvilibusiness@gmail.com>
2025-07-02 14:02:05 +0400

    Make Register and Login form look better

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,22 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
   imports: [
     RouterLink,
     FormsModule,
     HttpClientModule
   ],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css']
 })
 export class Register {
-  step: 'form' | 'verify' | 'done' = 'form';
-
-
   form = {
     username: '',
     email: '',
@@ -24,62 +22,16 @@ export class Register {
     password: ''
   };
 
-  verificationCode = "";
+  @Output() emailSubmitted = new EventEmitter<string>();
 
   constructor(private http: HttpClient) {}
 
-  goToVerificationStep() {
-    if (!this.form.email)
-    {
-      alert("Email is required");
+  onSubmit() {
+    const { username, email, birthdate, password } = this.form;
+
+    if (!username || !email || !birthdate || !password) {
+      alert("All fields are required");
       return;
     }
-
-    this.http.post('https://dlt-api.shotatevdorashvili.com/api/email/send-verification-code', {
-      email: this.form.email
-    }).subscribe({
-      next: () => {
-        this.step = 'verify';
-      },
-      error: err => {
-        alert('Failed to send verification code');
-        console.error(err);
-      }
-    });
-  }
-
-  verifyCode() {
-    if(!this.verificationCode)
-    {
-      alert("Enter the verification code")
-      return;
-    }
-
-    this.http.post('https://dlt-api.shotatevdorashvili.com/api/email/verify-code', {
-      email: this.form.email,
-      code: this.verificationCode
-    }).subscribe({
-      next: () => {
-        this.registerUser();
-      },
-      error: err => {
-        alert('Verification failed');
-        console.error(err);
-      }
-    });
-  }
-  
-  registerUser() {
-    this.http.post('https://dlt-api.shotatevdorashvili.com/api/auth/register', this.form)
-      .subscribe({
-        next: () => {
-          this.step = 'done';
-          alert('Registration successful!');
-        },
-        error: err => {
-          alert('Registration failed');
-          console.error(err);
-        }
-      });
   }
 }
